d170fcb290cc715f2443e11eaaffe57ed1e2f0d8
rbgks8617-w <rbgks8617@gmail.com>
2026-06-22 17:34:41 +0900

    Publish web prototype static build

--- scripts/prepare-gh-pages.js
+++ scripts/prepare-gh-pages.js
@@ -2,13 +2,12 @@ const fs = require("fs");
 const path = require("path");
 
 const outputDir = path.join(process.cwd(), "dist-web");
-const basePath = "/-BABMUKJA-project";
 const indexPath = path.join(outputDir, "index.html");
 
 let indexHtml = fs.readFileSync(indexPath, "utf8");
 indexHtml = indexHtml
-  .replaceAll('href="/favicon.ico"', `href="${basePath}/favicon.ico"`)
-  .replaceAll('src="/_expo/', `src="${basePath}/_expo/`);
+  .replaceAll('href="/favicon.ico"', 'href="./favicon.ico"')
+  .replaceAll('src="/_expo/', 'src="./_expo/');
 fs.writeFileSync(indexPath, indexHtml);
 
 const jsDir = path.join(outputDir, "_expo", "static", "js", "web");
@@ -18,7 +17,7 @@ for (const fileName of fs.readdirSync(jsDir)) {
   }
 
   const jsPath = path.join(jsDir, fileName);
-  const js = fs.readFileSync(jsPath, "utf8").replaceAll('httpServerLocation:"/assets/', `httpServerLocation:"${basePath}/assets/`);
+  const js = fs.readFileSync(jsPath, "utf8").replaceAll('httpServerLocation:"/assets/', 'httpServerLocation:"./assets/');
   fs.writeFileSync(jsPath, js);
 }
 
